539e6643ba0ab68aaaf3902a9974026d1b2aa83a
hiro <onecast.onelost615@gmail.com>
2023-02-13 13:16:40 +0900

    動作確認

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const text = document.querySelector(".text");
   const text2 = document.querySelector(".text2");
+  const text3 = document.querySelector(".text3");
   let str = text.innerHTML.trim().split("");
   let str2 = text2.innerHTML.trim().split("");
+  let str3 = text3.innerHTML.trim().split("");
 
   text.innerHTML = str.reduce((acc, curr) => {
     //渡ってきた半角スペースを&nbsp;へ置換し、再びcへ代入(/\s+/は正規表現の半角スペース)
@@ -16,27 +18,45 @@ document.addEventListener("DOMContentLoaded", () => {
     //cにstrの文字が一文字ずつ入ってくる
     return `${acc}<span class="char2">${curr}</span>`; //テンプレートリテラルで繋いだものをループさせて次々concatStrへ格納
   }, "");
+  text3.innerHTML = str3.reduce((acc, curr) => {
+    //渡ってきた半角スペースを&nbsp;へ置換し、再びcへ代入(/\s+/は正規表現の半角スペース)
+    curr = curr.replace(/\s+/, "&nbsp;");
+    //cにstrの文字が一文字ずつ入ってくる
+    return `${acc}<span class="char3">${curr}</span>`; //テンプレートリテラルで繋いだものをループさせて次々concatStrへ格納
+  }, "");
 
   const btn = document.querySelector(".button");
   let char = document.querySelector(".char").className;
   let char2 = document.querySelector(".char2").className;
+  let char3 = document.querySelector(".char3").className;
   btn.addEventListener("click", () => {
     console.log(char);
     gsap
       .timeline()
       .to(`.${char}`, {
-        // y: 0 /*テキストのY軸の操作*/,
-        opacity: 1,
-        stagger: 0.03 /*テキスト間の遅延時間*/,
+        y: 0 /*テキストのY軸の操作*/,
+        rotate: 0,
+        stagger: 0.02 /*テキスト間の遅延時間*/,
         delay: 0.2 /*アニメーションのスタートまでの遅延時間*/,
         duration: 0.3 /*アニメーションの時間*/,
       })
       .to(
         `.${char2}`,
         {
-          // y: 0 /*テキストのY軸の操作*/,
-          opacity: 1,
-          stagger: 0.03 /*テキスト間の遅延時間*/,
+          y: 0 /*テキストのY軸の操作*/,
+          rotate: 0,
+          stagger: 0.02 /*テキスト間の遅延時間*/,
+          delay: 0.2 /*アニメーションのスタートまでの遅延時間*/,
+          duration: 0.3 /*アニメーションの時間*/,
+        },
+        "-=0.8"
+      )
+      .to(
+        `.${char3}`,
+        {
+          y: 0 /*テキストのY軸の操作*/,
+          rotate: 0,
+          stagger: 0.02 /*テキスト間の遅延時間*/,
           delay: 0.2 /*アニメーションのスタートまでの遅延時間*/,
           duration: 0.3 /*アニメーションの時間*/,
         },
